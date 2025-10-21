0da9076ab561d6a3556f2a8d8ff4c9483759ead0
Emilly-Freire <emillyfreireluna17@gmail.com>
2025-10-21 11:04:06 -0300

    uiiiiiiiiii

--- prova_lyzbru/Back-Adonis/database/seeders/03_cliente_seeder.ts
+++ prova_lyzbru/Back-Adonis/database/seeders/03_cliente_seeder.ts
@@ -5,18 +5,18 @@ export default class ClienteSeeder extends BaseSeeder {
   public async run() {
     await Cliente.create({
       nome_completo: 'Bruna',
-      email: 'cliente1@teste.com',
+      email: 'cliente1@gmail.com',
       senha: '1234',
       cpf: '09922050925',
       cidade: 'Pontal do Paraná',
       estado: 'PR',
-      rua: 'Rua Nao aguento Mais',
+      rua: 'Rua porra de codigo, odio',
       numero_casa: '123',
     })
     
     await Cliente.create({
       nome_completo: 'Lyz',
-      email: 'cliente2@teste.com',
+      email: 'cliente2@gmail.com',
       senha: '1234',
       cpf: '03522050674',
       cidade: 'Matinhos',
